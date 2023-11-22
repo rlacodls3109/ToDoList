e7556c15be8342c1e1810d4b9ba714b4f2bde289
chaeinkim <32191199@dankook.ac.kr>
2023-11-23 01:06:43 +0900

    리액트랑 연동

--- ToDoList/todos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', todolist, name='list'),
-    path('create/', todocreate, name='create'),
-    path('update/<str:pk>/', todoupdate, name='update'),
-    path('delete/<str:pk>/', tododelete, name='delete')
-]
